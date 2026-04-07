ac252f2e208fe4310fcd9401845f2576a6f6e864
Zineb HANNANE <trainer@2e-training.com>
2026-04-07 14:35:55 +0100

    quick changes: phone number + keywords

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -192,7 +192,7 @@ export default function Layout() {
                 </li>
                 <li className="flex items-center gap-2 text-[#8892a6] text-sm">
                   <Phone className="w-4 h-4 text-[#00a8ff]" />
-                  +212 6 62 02 81 33
+                  +212 6 00 00 00 00
                 </li>
                 <li className="flex items-center gap-2 text-[#8892a6] text-sm">
                   <MapPin className="w-4 h-4 text-[#00a8ff]" />
